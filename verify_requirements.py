ea90d2885a60a323ead51ecf1e70d74846f50099
Luvsandondov Lkhamsuren <lkhamsurenl@stripe.com>
2023-06-18 10:51:17 -1000

    Setup requirements.txt

---
+++ verify_requirements.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+
+import numpy as np
+import pandas as pd
+  
+
+def main():
+	print(f"Numpy version: {np.__version__}")
+	print(f"Pandas version: {pd.__version__}")
+
+
+if __name__ == "__main__":
+    main()
