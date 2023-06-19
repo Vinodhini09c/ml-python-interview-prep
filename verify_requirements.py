b9ce5e4699a7ce69e558ad57fe34b1034f77ca1b
Luvsandondov Lkhamsuren <lkhamsurenl@stripe.com>
2023-06-19 09:34:23 -1000

    add python-interview-prep deps

--- verify_requirements.py
+++ verify_requirements.py
@@ -3,12 +3,15 @@
 import numpy as np
 import pandas as pd
 import pytest
+import requests
   
 
 def main():
 	print(f"Numpy version: {np.__version__}")
 	print(f"Pandas version: {pd.__version__}")
 	print(f"Pytest version: {pytest.__version__}")
+	print(f"Requests version: {requests.__version__}")
+
 
 
 if __name__ == "__main__":
